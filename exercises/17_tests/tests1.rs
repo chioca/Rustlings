3738005c63ade1bad3827c375cbfa3dcf6d24eef
chioca <Kraistine21@gmail.com>
2025-01-26 22:21:57 +0800

    16_lifetimes & 17_tests

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -7,17 +7,21 @@ fn is_even(n: i64) -> bool {
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}",is_even(2));
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::is_even;
+
     // TODO: Import `is_even`. You can use a wildcard to import everything in
     // the outer module.
-
     #[test]
     fn you_can_assert() {
         // TODO: Test the function `is_even` with some values.
-        assert!();
-        assert!();
+        assert!(is_even(2), "2 should be even");
+        assert!(!is_even(3), "3 should not be even");
+        assert!(is_even(4), "4 should be even");
+        assert!(!is_even(5), "5 should not be even");
     }
 }
